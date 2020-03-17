afd6da24f519672a43ec210628b8d2444e69bf42
anieo <ali.maher.1460@gmail.com>
2020-03-17 01:26:22 +0000

    first commmit

---
+++ scripts/inference.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ -n $1 ]; then
+    output = $1
+else
+    output="output"
+mkdir -p "$output/"
+python inference.py --tacotron2 "$output/checkpoint_Tacotron2_4" --waveglow "$output/checkpoint_WaveGlow_6" -o "$output/" -i phrases/phrase.txt --amp-run
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+pip install unidecode
+pip install "git+https://github.com/NVIDIA/dllogger"
+export CUDA_HOME=/usr/local/cuda-10.1
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" "git+https://github.com/NVIDIA/apex"
