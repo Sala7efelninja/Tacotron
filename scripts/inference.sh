37b5a2e85aa197a8f934f542387b6fef3ddc7c6b
anieo <ali.maher.1460@gmail.com>
2020-03-17 01:31:36 +0000

    first commmit

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -3,5 +3,6 @@ if [ -n $1 ]; then
     output = $1
 else
     output="output"
+fi
 mkdir -p "$output/"
 python inference.py --tacotron2 "$output/checkpoint_Tacotron2_4" --waveglow "$output/checkpoint_WaveGlow_6" -o "$output/" -i phrases/phrase.txt --amp-run
--- scripts/train_tacotron2.sh
+++ scripts/train_tacotron2.sh
@@ -3,6 +3,7 @@ if [ -n $1 ]; then
     output = $1
 else
     output="output"
+fi
 mkdir -p "$output/"
 --epochs-per-checkpoint 50
 # python -m multiproc train.py -m Tacotron2 -o ./output/ --training-files filelists/ljs_audio_text_train_subset_625_filelist.txt -lr 1e-3 --epochs 1501 -bs 104 --weight-decay 1e-6 --grad-clip-thresh 1.0 --cudnn-enabled --log-file nvlog.json --anneal-steps 500 1000 1500 --anneal-factor 0.1 --amp-run
